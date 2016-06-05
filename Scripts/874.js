877556c6204f9f42a5c0a227146975717392fa9f
christinedenamur <denamurchristine@gmail.com>
2016-06-05 20:07:18 +0200

    20-07

--- Scripts/874.js
+++ Scripts/874.js
@@ -1,4 +1,15 @@
-﻿$(document).ready(function(){
+﻿// IE10+
+document.getElementsByTagName( "html" )[0].classList.remove( "loading" );
+
+// All browsers
+document.getElementsByTagName( "html" )[0].className.replace( /loading/, "" );
+
+// Or with jQuery
+$( "html" ).removeClass( "loading" );
+
+
+
+$(document).ready(function(){
     animateDiv();
     
 });
